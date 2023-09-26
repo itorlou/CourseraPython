c40ea61b8b0188a6d8d50718b9068a30e1ca3328
Isaac Torrado <isaactorlou@gmail.com>
2023-09-26 15:24:18 +0000

    quitamos los pass

--- Crash Course On Python/Week 5/OOP/LabElevator.py
+++ Crash Course On Python/Week 5/OOP/LabElevator.py
@@ -4,24 +4,19 @@ class Elevator:
         self.bottom=bottom
         self.top=top
         self.current=current
-        pass
     def up(self):
         """Makes the elevator go up one floor."""
         if self.current+1<self.top:
             self.current+=1
-        pass
     def down(self):
         """Makes the elevator go down one floor."""
         if self.current-1>self.bottom:
             self.current-=1        
-        pass
     def go_to(self, floor):
         """Makes the elevator go to the specific floor."""
         self.current = floor
-        pass
     def __str__(self):
         return "Current floor: {}".format(self.current)
-        pass
     
 elevator = Elevator(-1, 10, 0)
 
