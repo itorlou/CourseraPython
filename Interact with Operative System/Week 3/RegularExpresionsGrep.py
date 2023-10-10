a0fd2bc86c5cedb8b63bc5a40f4daa2691472e4b
Isaac Torrado <isaactorlou@gmail.com>
2023-10-10 10:22:33 +0000

    grep

--- Interact with Operative System/Week 3/RegularExpresionsGrep.py
+++ Interact with Operative System/Week 3/RegularExpresionsGrep.py
@@ -6,3 +6,43 @@ from os.path import dirname, join
 
 current_dir = dirname(__file__)
 file_path = join(current_dir, "./loren.txt")
+
+# @itorlou ➜ /workspaces/CourseraPython/Interact with Operative System/Week 3 (main) $ grep at loren.txt
+
+# exercitation
+# consequat
+# voluptate
+# fugiat
+# pariatur
+# occaecat
+# cupidatat
+
+# para que no sea case sensitive podemos usar -i 
+
+# buscamos patrones con el .
+
+# @itorlou ➜ /workspaces/CourseraPython/Interact with Operative System/Week 3 (main) $ grep n.m loren.txt 
+# enim
+# minim
+# anim
+
+# ^indica el inicio 
+# 
+# @itorlou ➜ /workspaces/CourseraPython/Interact with Operative System/Week 3 (main) $ grep ^s  loren.txt 
+# sit
+# sed
+# sint
+# sunt
+
+
+# $ indica el final
+
+# @itorlou ➜ /workspaces/CourseraPython/Interact with Operative System/Week 3 (main) $ grep m$ loren.txt 
+# Lorem
+# ipsum
+# enim
+# minim
+# veniam
+# cillum
+# anim
+# laborum
