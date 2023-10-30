de4e78b9c8cae5385713f0a2a645bc23c870218a
Isaac Torrado <isaactorlou@gmail.com>
2023-10-30 17:06:55 +0000

    logging

---
+++ Interact with Operative System/Week 4/Processing Log/FilteringLogs.py
@@ -0,0 +1,12 @@
+import re
+import sys
+
+logfile = sys.argv[1]
+
+with open(logfile) as f:
+    for line in f:
+        if "CRON" not in line:
+            continue
+        pattern = r"USER \((\w+)\)$"
+        result = re. search(pattern, line)
+        print(result[1])
